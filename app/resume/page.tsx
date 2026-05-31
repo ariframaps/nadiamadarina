b9673bc4acd2f32f9d6b694b249da62014d96961
ariframaps <ariframaputra33@gmail.com>
2026-06-01 01:48:13 +0200

    resume page

---
+++ app/resume/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+
+export default function ResumePage() {
+	redirect(
+		"https://docs.google.com/document/d/1z_aALgt6O2QIE7YUk5XRmg5Ba00JfGZW/preview",
+	);
+}
